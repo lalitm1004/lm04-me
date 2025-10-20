27f1dac514e592749a28fb2cd25d0b5d67dc23b2
lalitm1004 <lalitm1004@gmail.com>
2025-10-20 22:27:30 +0530

    feat: finish /projects

---
+++ src/components/Star.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { Tooltip } from "bits-ui";
+
+    let { is_important }: { is_important: boolean } = $props();
+</script>
+
+{#if is_important}
+    <Tooltip.Provider>
+        <Tooltip.Root delayDuration={200}>
+            <Tooltip.Trigger class={`absolute top-4 right-4 cursor-pointer`}>
+                {@render starSvg()}
+
+                <p class={`sr-only`}>Featured Project!</p>
+            </Tooltip.Trigger>
+
+            <Tooltip.Content sideOffset={8} side={`left`}>
+                <div
+                    class={`z-50 bg-stone-950 border-2 border-neutral-800 px-2 py-1 rounded-md text-sm`}
+                >
+                    Featured Project!
+                </div>
+            </Tooltip.Content>
+        </Tooltip.Root>
+    </Tooltip.Provider>
+{/if}
+
+{#snippet starSvg()}
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="h-[24px] aspect-square fill-stone-700 group-hover:rotate-[72deg] transition-transform duration-300 lucide lucide-star-icon lucide-star"
+    >
+        <path
+            d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"
+        />
+    </svg>
+{/snippet}
